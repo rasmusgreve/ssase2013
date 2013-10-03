37c909c185020f3e9084bb64b644fa6f95417501
chrilyng <chrilyng@gmail.com>
2013-10-03 22:57:09 +0100

    changed to support the enum

--- table_data.sql
+++ table_data.sql
@@ -1,1 +1,1 @@
-﻿INSERT INTO student VALUES(1, 'admin', 'Centauri', 'alpha@centauri.uni','c20fe9a7ebe0286842467d7845b9115a04e2201b3468c4f61e4f8d00aca5d8b1', '2011-11-11', 'closed', ']``a', true);
+﻿INSERT INTO student VALUES(1, 'admin', 'Centauri', 'alpha@centauri.uni','c20fe9a7ebe0286842467d7845b9115a04e2201b3468c4f61e4f8d00aca5d8b1', '2011-11-11', '2', ']``a', true);
