97208412004d4b1b67b4fb90c1f43a4c1a9b00be
chrilyng <chrilyng@gmail.com>
2013-11-27 14:06:08 +0100

    Related to issue 17 Makes it possible to "delete" users

--- table_data.sql
+++ table_data.sql
@@ -1,4 +1,4 @@
-﻿INSERT INTO student VALUES(1, 'Admin', 'Nilson', 'admin', 'Centauri', '77e05fb19ddc6b52c4902689b662f76fe9f7097e4ea4468ed65716596f6026a1', ']^\]', true);
+﻿INSERT INTO student VALUES (1, 'Admin', 'Nilson', 'admin', 'Centauri', '77e05fb19ddc6b52c4902689b662f76fe9f7097e4ea4468ed65716596f6026a1', ']^\]', true, false);
 
 SELECT setval('public.student_id_seq', 1, true);
 
