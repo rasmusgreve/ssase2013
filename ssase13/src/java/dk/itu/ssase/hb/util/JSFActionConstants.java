3cae4448e44c1d37394ec7e40e80e7eb60d4e3ba
chrilyng <chrilyng@gmail.com>
2013-12-15 05:12:07 +0100

    Forgot class, sorry

---
+++ ssase13/src/java/dk/itu/ssase/hb/util/JSFActionConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.itu.ssase.hb.util;
+
+/**
+ *
+ * @author christian
+ */
+public class JSFActionConstants {
+    public static final String JSFSuccess = "success";
+    public static final String JSFFailure = "failure";
+    public static final String JSFLogin = "login";
+}
